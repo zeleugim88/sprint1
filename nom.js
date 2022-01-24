72d8c0543ee87f3c73e83b52e74b89cbd113020a
DANI MI <zeleugim@gmail.com>
2022-01-24 18:07:34 +0100

    nivell 1 exercici 1

---
+++ nom.js
@@ -0,0 +1,6 @@
+
+//Nivell 1 - Exercici 1
+//Crea una funció que mostri per consola el nom d'usuari al invocar-la passant-li el nom com a paràmetre.
+let nom = "Pepito";
+const showName = x => console.log(x);
+showName(nom);
