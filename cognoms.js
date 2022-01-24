eaf8d63505a42e58e9dc7dcb4b0e5025608e9770
DANI MI <zeleugim@gmail.com>
2022-01-24 18:16:00 +0100

    nivell 2 - exercici 1

---
+++ cognoms.js
@@ -0,0 +1,7 @@
+//Nivell 2 - Exercici 1
+//Mostra per consola el nom i cognoms de l'usuari mitjançant template literals, 
+//guardant-los en variables i referenciant-les en la impressió del missatge.
+let nom = "Pepito";
+let cognom = "Perez";
+const showNames = (x,y) => console.log(`My name is ${x} and my surname is ${y}`);
+showNames(nom,cognom);
