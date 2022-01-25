76698f52027f1bb17cf3f54cab259bd564f1699b
Daniel Miguelez <zeleugim@gmail.com>
2022-01-25 10:47:28 +0100

    n2-e1

---
+++ entrega 2/n2-e1.js
@@ -0,0 +1,6 @@
+//Nivell 2 - Exercici 1
+//Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut.
+
+
+const f = (parametre) => ({atribut : parametre});
+console.log(f("valor"));
