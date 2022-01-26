804e6a5d5bb6dccf470ad70b68d961792b65696b
zeleugim88 <65772377+zeleugim88@users.noreply.github.com>
2022-01-26 16:41:13 +0100

    Update entrega1.js

--- entrega1.js
+++ entrega1.js
@@ -15,14 +15,19 @@ showNames(nom,cognom);
 
 //Nivell 2 - Exercici 2
 //Invoca una funció que retorni un valor des de dins d'una template literal.
-const suma = (x,y) => `La suma de ${x} y ${y} es ${x*y}`;
-console.log(suma(2,5));
+/* const suma = (x,y) => `La suma de ${x} y ${y} es ${x*y}`;
+console.log(suma(2,5)); */
+//CORRECCIÓ 26/01/2022 => Al N2 E2 has d'executar una funció que retorni un valor DINS d'un template literal, no al reves
+const t1 = `Bon día. `;
+const t2 = `Com estàs?`;
+const showConcatenar = (x,y) => console.log(x + y);
+showConcatenar(t1,t2);
 
 
 //Nivell 3 - Exercici 1
 //Crea una matriu de deu funcions i emplena-la mitjançant un bucle de manera que cada funció compti del 0 al 9 per la consola. 
 //Invoca cada funció de l'array iterativament. Haurà de mostrar-se per consola el compte del 0 al 9 deu vegades.
-const f0 = function() { return console.log(0) };
+/* const f0 = function() { return console.log(0) };
 const f1 = function() { return console.log(1) };
 const f2 = function() { return console.log(2) };
 const f3 = function() { return console.log(3) };
@@ -33,8 +38,15 @@ const f7 = function() { return console.log(7) };
 const f8 = function() { return console.log(8) };
 const f9 = function() { return console.log(9) };
 const arr = [f0(),f1(),f2(),f3(),f4(),f5(),f6(),f7(),f8(),f9()];
-for (x in arr) {return};
-
+for (x in arr) {return}; */
+//CORRECCIÓ 26/01/2022 => El N3 E1 no fa el que diu l'enunciat! has d'omplir i llegir l'array amb bucles, no a mà 
+const matriu = [];
+const arr = [0,1,2,3,4,5,6,7,8,9];
+const f = () => arr.map( x => console.log(x));
+for (let i = 0; i <= 9; i++) {
+    matriu.push(f)
+    matriu[i]()
+}
 
 //- Nivell 3 - Exercici 2
 //Crea una funció anònima autoinvocable igualada a una variable que mostri per consola el nom de l'usuari rebut com a paràmetre.
