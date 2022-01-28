cae3848c8a669a3f7d800d46f779a4c4a2b04f72
Daniel Miguelez <zeleugim@gmail.com>
2022-01-28 14:23:50 +0100

    temporal update

--- temporal.js
+++ temporal.js
@@ -24,25 +24,58 @@ let salaries = [{
 }];
 
 //crea una arrow function getEmployee que retorni una Promise efectuant la cerca en l'objecte pel seu id. 
-
-
-const getEmployee = (id) => {
+const getEmployee = (id) => { 
     return new Promise ((resolve, reject) =>{
         resolve(employees.filter(employees => employees.id === id))
     })
 }
 //console.log(getEmployee(2));
+//getEmployee(1).then(x => console.log(x));
 
-const obj = {
+//console.log(getEmployee(2));
+/* const obj = {
     id: 3,
     name: 'Jeff Bezos'
-};
+}; */
 
-//Crea una altra arrow function getSalary que rebi com a paràmetre un objecte employee i retorni el seu salari.dfsf
-
+//Crea una altra arrow function getSalary que rebi com a paràmetre un objecte employee i retorni el seu salari
 const getSalary = (obj) => {
     return new Promise ((resolve, reject) => {
         const foundObject = salaries.filter(salaries => salaries.id === obj.id);
         resolve(foundObject[0].salary);
     })
 }
+
+//console.log(getSalary(obj))
+
+//- Exercici 2
+//Crea una funció asíncrona que rebi un id d'empleat i imprimeixi per pantalla el nom de l'empleat i el seu salari, usant les funcions que has definit a l'exercici anterior.
+async (id) => {
+    const promise1 = await getEmployee(2);
+    console.log(promise1)
+    const promise2 = await console.log(getSalary(promise1))
+    console.log(promise2)
+}
+
+
+//Nivell 2 - Exercici 1
+//Crea una nova funció asíncrona que cridi a una altra que retorni una Promise que efectuï la seva funció resolve() després de 2 segons de la seva invocació.
+const fPromise = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {resolve("Vamoss");}, 2000)
+    }) 
+}
+
+const fAsincrona = async () => {
+    try {
+        await console.log(fPromise())
+      }
+      catch(err) {
+        "Ohh shit"
+      } 
+}
+
+fAsincrona();
+
+/* Nivell 3 - Exercici 1
+Captura tots els errors possibles dels nivells 1 i 2. */
