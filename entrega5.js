6b8de0ba424fb0f671b38715f8023c4c221f499d
zeleugim88 <65772377+zeleugim88@users.noreply.github.com>
2022-02-01 08:41:53 +0100

    Update entrega5.js

--- entrega5.js
+++ entrega5.js
@@ -11,12 +11,14 @@ const demores1segon = function () {
 //=======================================================================================================================================
 //Nivell 1 - Exercici 2
 //Crea una funció que, en executar-la, escrigui una frase en un fitxer.
-
+//Opció síncrona
 const fs1 = require('fs');
-const fileName = 'test.txt'
-const content = '<<<<<<<<<<<<<<<<<<<Awesome Text>>>>>>>>>>>>>>>>>>>>>'
-//mirar writeFilesync para ahorrar líneas*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-const fEscriure = () => {
+const fEscriure = () => fs1.writeFileSync('test.txt','Awesome text')
+//fEscriure ();
+
+
+//Opció asíncrona
+/* const fEscriure = () => {
     fs1.writeFile(fileName, content, err => {
         if (err) {
             console.error(err)
@@ -24,19 +26,21 @@ const fEscriure = () => {
         }
         console.log(`Archivo ${__dirname}\\${fileName} modificado exitosamente con el texto ${content} `)
     })
-}
+} */
 
-//fEscriure ();
+
 
 
 //=======================================================================================================================================
 //Nivell 1 - Exercici 3
 //Crea una altra funció que mostri per consola el contingut del fitxer de l'exercici anterior.
 
-const fs2 = require('fs')
-const filePath = 'C:\\Users\\formacio\\Documents\\danimi\\sprint1\\test.txt';
+const fs2 = require('fs');
+const fLlegir = () => console.log(fs2.readFileSync('./test.txt',{encoding:'utf8', flag:'r'}));
+//fLlegir();
+
 //mirar readFilesync para ahorrar líneas*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-const fLlegir = () => {
+/* const fLlegir = () => {
     fs2.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             console.error(err)
@@ -44,10 +48,7 @@ const fLlegir = () => {
         }
         console.log(data)
     })
-}
-
-//fLlegir();
-
+} */
 
 //=======================================================================================================================================
 // Nivell 2 - Exercici 1
@@ -71,13 +72,22 @@ const fComprimir = () => {
 
 // Nivell 2 - Exercici 2
 // Crea una funció que llisti per la consola el contingut del directori d'usuari de l'ordinador utilizant Node Child Processes.
-// The child_process module provides the ability to spawn subprocesses
-// Spawn in computing refers to a function that loads and executes a new child process
+// The child_process module provides the ability to spawn subprocesses (load and execute a new child process)
 
-//const filePath2 = process.argv[1]
-const filePath2 = `C:\\Users\\formacio\\Documents\\danimi\\sprint1`
+//OPCIÓ 1: Node Child Processes
+const displayFiles = () => {
+    const cp = require('child_process');
+    //const exec_options = {}
+    //cp.exec('dir',exec_options, (err, stdout, stderr) =>
+    cp.exec('dir', (err, stdout, stderr) => { ////The dir command displays a list of files and subdirectories in a directory in Windows. In Linux is ls
+        console.log(stdout)
+    });
+}
+//displayFiles();
 
-/* var fs4 = require('fs');
+//OPCIÓ 2: NODE FILE SYSTEM
+/*const filePath2 = `C:\\Users\\formacio\\Documents\\danimi\\sprint1`
+var fs4 = require('fs');
  
 fs4.readdir(filePath2, function(err, items) {
     console.log(items);
@@ -87,11 +97,27 @@ fs4.readdir(filePath2, function(err, items) {
     }
 }); */
 
-//The dir command displays a list of files and subdirectories in a directory in Windows. In Linux is ls
-const cp = require('child_process');
+/* Nivell 3 - Exercici 1
+Crea una funció que creï dos fitxers codificats en hexadecimal i en base64 respectivament, a partir del fitxer del nivell 1
+Crea una funció que guardi els fitxers del punt anterior, ara encriptats amb l'algorisme aes-192-cbc, i esborri els fitxers inicials
+Crea una altra funció que desencripti i descodifiqui els fitxers de l'apartat anterior tornant a generar una còpia de l'inicial
+Inclou un README amb instruccions per a l'execució de cada part */
+const fs5 = require('fs');
+const path = __dirname; 
+const testText = fs5.readFileSync('./test.txt',{encoding:'utf8', flag:'r'});
+let base64data = buff.toString('base64');
+const testTextHex = testText.toString(16); //toString('hex')
+const testText64 = testText.toString('base64');
 
-//const exec_options = {}
-//cp.exec('dir',exec_options, (err, stdout, stderr) =>
-cp.exec('dir', (err, stdout, stderr) => {
-console.log(stdout)
-});
+/* const fs2 = require('fs')
+const filePath = 'C:\\Users\\formacio\\Documents\\danimi\\sprint1\\test.txt';
+//mirar readFilesync para ahorrar líneas*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+const fLlegir = () => {
+    fs2.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        console.log(data)
+    })
+} */
